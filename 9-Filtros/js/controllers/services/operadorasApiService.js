cf742aeda29206e850b32ba2f391e4cc4761a8fb
luancalais@gmail.com <luancalais@gmail.com>
2022-01-10 20:18:16 -0300

    Criando diretivas parte 1

--- 9-Filtros/js/controllers/services/operadorasApiService.js
+++
@@ -1,8 +0,0 @@
-// SERVICE é um tipo de serviço similar ao construtor
-angular.module('listaTelefonica').service('operadorasAPI', function ($http, config) {
-
-    // Construtora SERVICE 
-    this.getOperadoras = function () {
-        return $http.get(config.baseURL + "/operadoras")
-    }
-});

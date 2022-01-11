a75d1f21e9d943ed88523d988c11a6cc16c23e14
luancalais@gmail.com <luancalais@gmail.com>
2022-01-11 10:56:09 -0300

    criando filtro de primeira letra uppercase e ellipsis

--- 9-Filtros/js/filters/ellipsisFilter.js
+++ 9-Filtros/js/filters/ellipsisFilter.js
@@ -1,7 +1,11 @@
 angular.module("listaTelefonica").filter("ellipsis", function () {
+	// Size é o tamanho mínimo definido na chamada para a partir dele adicionar os "..."
 	return function (input, size) {
+		// Regra para que não se aplique o filtro caso o input seja menor que o size definido
 		if (input.length <= size) return input;
-		var output = input.substring(0,(size || 2)) + "...";
+		// output recebe o nome formatado tirando 2 do tamanho total e adicionando "..."
+		// (size || 2) = Se o size for evaluado como false ele faz || cai do lado 2 e retorna de 0 a 2, se for true fica de 0 a size
+		var output = input.substring(0, (size || 2)) + "...";
 		return output;
 	};
 });
--- 9-Filtros/js/filters/nameFilter.js
+++ 9-Filtros/js/filters/nameFilter.js
@@ -1,12 +1,22 @@
 angular.module("listaTelefonica").filter("name", function () {
+
+	// Input é o que estará sendo aplicado o filtro
 	return function (input) {
+
+		// Cria uma lista em cada elemento entre um espaço em branco
 		var listaDeNomes = input.split(" ");
+
+		// .map -> pega a lista de nomes e para cada elemento(nome) e aplica o que for definido
 		var listaDeNomesFormatada = listaDeNomes.map(function (nome) {
-			if(nome.length <= 3) {
-            			if(/(da|de|do|das|dos)/.test(nome)) return nome;
-         		}
+
+			// Verfica se a entrada é "da,de,do" e mantem minúsculo (Estamos usando uma expressão regular)
+			if (nome.length <= 3) {
+				if (/(da|de|do|das|dos)/.test(nome)) return nome;
+			}
+			// Caracter na posição 0 para uppercase concatenado com o resto da palava em lower case
 			return nome.charAt(0).toUpperCase() + nome.substring(1).toLowerCase();
 		});
+		// Pega o array e coloca um espaço entre cada elemento transformando em uma string de novo (processo inverso ao split)
 		return listaDeNomesFormatada.join(" ");
 	};
-});
+}); 
